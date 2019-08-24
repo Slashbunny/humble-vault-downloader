b7a96fd308cf115906601f3a0d569a2f957f343d
slashbunny <Slashbunny@users.noreply.github.com>
2019-08-24 12:39:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Humble Trove Downloader
+
+Quick and dirty script to download all games, for all operating
+systems, from [Humble Trove](https://www.humblebundle.com/monthly/trove). You
+need a Humble Monthly subscription to download these games.
+
+Existing files have their md5 checksums checked versus what the Humble Bundle API
+reports. If there is a mismatch, you'll need to delete or remove the offending
+file and run the script again.
+
+This script was meant for archival purposes, so it does not have way to filter
+what games are downloaded.
+
+# Setup
+
+## Pre-requisites
+
+Install [PHP](https://www.php.net/) and [Composer](https://getcomposer.org/). If
+you are using Linux, these should be available from your distribution's package
+repositories.
+
+## Install Dependencies
+
+After cloning this repository, run `composer` in the project's main directory
+ to install dependencies:
+
+```bash
+$ composer install
+```
+
+or, if you downloaded `composer.phar` separately:
+
+```bash
+$ php composer.phar install
+```
+
+# Usage
+
+## API Key
+
+See the [instructions here](https://github.com/talonius/hb-downloader/wiki/Using-Session-Information-From-Windows-For-hb-downloader) on
+how to get a session key from your browser while logged into the Humble Bundle website. This script will not work without
+it.
+
+## Running the Script
+
+```bash
+$ ./src/download.php DOWNLOAD_PATH HUMBLE_API_KEY
+```
+
+or, if PHP is not available in your path:
+
+```bash
+$ /path/to/php src/download.php DOWNLOAD_PATH HUMBLE_API_KEY
+```
+
+Example (this is not a real key):
+
+```bash
+$ ./src/download.php /home/downloads/trove "eyJ1GzZJE0oqwEaunyOYX3yrlkFUxPJq8PFWCgkKOHM00\075|1566665561|JR7m2nO769sO2Je4C2fE"
+```
+
+# Help
+
+### Error Message: "Trying to get property 'signed_url' of non-object"
+
+Your API key is wrong. Double check is it correct and quote it the same way in the example under *Usage*.
+
---
+++ src/download.php
@@ -0,0 +1,201 @@
+#!/usr/bin/env php
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+set_error_handler('catchError');
+
+// Program takes 2 parameters
+if ($argc !== 3) {
+    printUsage($argv);
+}
+
+// Parameters to script
+$base_path = $argv[1];
+$trove_key = $argv[2];
+
+// Ensure the provided path is valid
+if (!is_dir($base_path)) {
+    echo "ERROR: " . $base_path . " is not a valid directory or does not exist! \n";
+    echo "       Create the directory and try again.\n\n";
+
+    printUsage($argv);
+}
+
+// Trove HTTP Client
+$client = getGuzzleHttpClient($trove_key);
+
+// Get list of all trove games from the API
+$trove_data = getTroveData($client);
+
+$count = 0;
+
+foreach ($trove_data as $game) {
+    $display   = $game->{'human-name'};
+    $downloads = $game->downloads;
+
+    echo "Processing $display ...\n";
+
+    foreach ($downloads as $os => $dl) {
+        $file = $dl->url->web;
+        $game = $dl->machine_name;
+        $md5  = $dl->md5;
+
+        $dl_path = $base_path . DIRECTORY_SEPARATOR . $os . DIRECTORY_SEPARATOR . $file;
+
+        // Ensure full path exists on disk
+        if (!is_dir(dirname($dl_path))) {
+            mkdir(dirname($dl_path), 0777, true);
+        }
+
+        echo "   Checking $os ($file)\n";
+
+        // File already exists- Check md5
+        if (file_exists($dl_path)) {
+            echo "    $file already exists!\n";
+            $existing_md5 = md5_file($dl_path);
+
+            if ($existing_md5 === $md5) {
+                echo "        Matching md5sum $md5 at $dl_path \n";
+                continue;
+            } else {
+                echo "        Wrong md5sum ($md5 vs $existing_md5) at $dl_path \n\n";
+                echo "Delete or move the existing file, then run this script again!\n\n";
+                exit(1);
+            }
+        } else {
+            echo "    $file does not exist\n";
+        }
+
+        echo "    Downloading to $dl_path... \n";
+
+        $url = getDownloadLink($client, $game, $file);
+
+        // Download file
+        $client->request(
+            'GET',
+            $url,
+            [
+                'sink'     => $dl_path,
+                'progress' => function(
+                    $downloadTotal,
+                    $downloadedBytes,
+                    $uploadTotal,
+                    $uploadedBytes
+                ) {
+                    if ($downloadTotal === 0) {
+                        $pct = 0;
+                    } else {
+                        $pct = round(($downloadedBytes / $downloadTotal) * 100, 2);
+                    }
+
+                    echo "\r    Progress: " . $pct . '%';
+                }
+            ]
+        );
+
+        echo "\n";
+
+        $count++;
+    }
+}
+
+echo "Downloaded $count games\n";
+
+/**
+ * Prints usage of script
+ */
+function printUsage($argv) {
+    echo "Usage: $argv[0] DOWNLOAD_PATH HUMBLE_API_KEY \n\n";
+    echo "    DOWNLOAD_PATH  - Base path to download files\n";
+    echo "    HUMBLE_API_KEY - Session from your browser's cookies\n\n";
+
+    exit(1);
+}
+
+
+/**
+ * Creates a Guzzle HTTP Client for interacting with the HB API
+ */
+function getGuzzleHttpClient($session_key)
+{
+    $cookies = [
+        '_simpleauth_sess' => '"' . $session_key . '"',
+    ];
+
+    $cookie_jar = \GuzzleHttp\Cookie\CookieJar::fromArray(
+        $cookies, 'humblebundle.com'
+    );
+
+    $client = new GuzzleHttp\Client([
+        'base_uri' => 'https://www.humblebundle.com/api/v1/',
+        'cookies'  => $cookie_jar,
+    ]);
+
+    return $client;
+}
+
+
+/**
+ * Gets data for Trove from the HB API
+ */
+function getTroveData($client)
+{
+    $page_num   = 1;
+    $trove_data = [];
+
+    while (true) {
+        echo "Fetching game list (page: $page_num)\n";
+
+        // Download each page of trove results
+        $page_data = json_decode(
+            $client->request('GET', 'trove/chunk?index=' . $page_num)->getBody()
+        );
+
+        // If results are empty, return data
+        if (empty($page_data)) {
+            return $trove_data;
+        }
+
+        // Combine results
+        $trove_data = array_merge($trove_data, $page_data);
+
+        $page_num++;
+
+        // Prevent possible endless loop if something changes with the API
+        if ($page_num > 10) {
+            echo "We fetched over 10 pages- Something may be wrong- Exiting\n";
+            exit(1);
+        }
+    }
+}
+
+
+/**
+ * Returns download URL for given user, game and file (win, mac, linux, etc)
+ */
+function getDownloadLink($client, $game, $file) {
+
+    $result = json_decode(
+        $client->request('POST', 'user/download/sign',
+            [
+                'form_params' => [
+                    'machine_name' => $game,
+                    'filename'     => $file,
+                ],
+            ]
+        )->getBody()
+    );
+
+    return $result->signed_url;
+}
+
+
+/**
+ * Handle any notices/warnings/errors
+ */
+function catchError($errNo, $errStr, $errFile, $errLine) {
+    echo "$errStr in $errFile on line $errLine\n";
+
+    exit(1);
+}
